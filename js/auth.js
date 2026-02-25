78900629b1a5bb1ed88f6e8b9e81bde384d8c4e3
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-02-25 23:57:45 +0100

    build signup page ui reusing login layout

--- js/auth.js
+++ js/auth.js
@@ -2,3 +2,9 @@ function guestLogin() {
     localStorage.setItem('currentUser', 'guest');
     window.location.href = 'summary.html';
 }
+
+function toggleSignupBtn() {
+    const privacyCheckbox = document.getElementById('privacyPolicy');
+    const signupBtn = document.getElementById('signupBtn');
+    signupBtn.disabled = !privacyCheckbox.checked;
+}
