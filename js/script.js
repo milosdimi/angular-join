206815e019eee6c3c9dcbab8b501acdb0bfc6b89
milosdimi <132084913+milosdimi@users.noreply.github.com>
2026-02-26 10:55:49 +0100

    templates created header sidebar

---
+++ js/script.js
@@ -0,0 +1,28 @@
+async function init() {
+    await includeHTML();
+    highlightActiveMenu();
+}
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
+function highlightActiveMenu() {
+    let url = window.location.pathname;
+    let links = document.querySelectorAll('.menu-link');
+    links.forEach(link => {
+        if (url.includes(link.getAttribute('href'))) {
+            link.classList.add('active');
+        }
+    });
+}
